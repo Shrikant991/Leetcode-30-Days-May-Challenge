3c3deb4958e6abbb5224055ef5da251744d00eb4
Shrikant Bhardwaj <48084675+Shrikant991@users.noreply.github.com>
2020-05-14 00:14:21 +0530

    First Commit

---
+++ Week 2/RemoveKDigits.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void removeTrailingZeroes(string &num)
+    {
+        while(num.length() > 0 && (num.compare(0, 1, "0") == 0))
+        {
+            num.erase(num.begin()+0);
+        }
+    }
+    void removeRemains(string &num, int k)
+    {
+        int i;
+        i = num.length() - k;
+        num.erase(num.begin()+i, num.end());
+    }
+
+    string removeKdigits(string num, int k)
+    {
+        int i = 0;
+        while((i < num.length()-1) && (k>0))
+        {
+            if(num[i] <= num[i+1])
+                i++;
+            else
+            {
+                num.erase(num.begin()+i);
+                k--;
+                if(i > 0)
+                    i--;
+            }
+        }
+        if(k!=0)
+            removeRemains(num, k);
+        if(num.compare(0,1,"0") == 0)
+            removeTrailingZeroes(num);
+        if(num == "")
+            return "0";
+        return num;
+    }
+};
